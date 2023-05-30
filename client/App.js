c10669e3951285b19590b654d418a5935425561a
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-30 15:08:31 -0500

    union

--- client/App.js
+++ client/App.js
@@ -1,36 +1,20 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-import React from "react";
-import Navigation from "./Navigation";
+import Login from './components/login.js';
+import Home from './components/home';
 
-
-// import Login from './components/login.js';
-// import Home from './components/home';
-
-
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-  //   <NavigationContainer>
-  //   <Stack.Navigator>
-  //     <Stack.Screen name="Login" component={Login} />
-  //     <Stack.Screen name="Home" component={Home} />
-  //     {/* Agrega más pantallas aquí */}
-  //   </Stack.Navigator>
-  // </NavigationContainer>
-  <Navigation/>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+      {/* Agrega más pantallas aquí */}
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- client/components/Navbar/Navbar.js
+++ client/components/Navbar/Navbar.js
@@ -1,19 +1,34 @@
-import React from "react";
-import { View, Text,TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Alert, TextInput } from "react-native";
 import { styles } from "./styles";
 
 function Navbar() {
+  const [proyect, setProyect] = useState("");
+  const handlesConfig = () => {
+    Alert.alert("Configuraciones");
+  };
   return (
     <>
-    <Text style={styles.text}>Luis Fernando ospina   </Text>
-    <Text style={styles.text}>Luis@gmail.com </Text>
-<TouchableOpacity
-style={styles.buton}
->
-<Text style={styles.textButon}>...</Text>
-</TouchableOpacity>
-     </>
+      <View>
+        <Text style={styles.text}>Luis Fernando ospina </Text>
+        <Text style={styles.text}>Luis@gmail.com </Text>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.buton} onPress={handlesConfig}>
+          <Text style={styles.textButon}>...</Text>
+        </TouchableOpacity>
+       </View>
+
+    
+    </>
   );
 }
-
+{
+  /* <TextInput
+style={styles.input}
+        placeholder="Busca tu proyecto aqui ....."
+        value={proyect}
+        onChangeText={setProyect}
+      /> */
+}
 export default Navbar;
