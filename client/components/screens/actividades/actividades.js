9c0bb3c1e769046710c102d6fccc87a7c2a0c225
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-31 19:18:30 -0500

    union miguel estiven

--- client/components/screens/actividades/actividades.js
+++ client/components/screens/actividades/actividades.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 import Constants from "expo-constants";
-
+import Navbar from '../../Navbar/Navbar';
 import WelcomeBar from '../../welcomeBar';
 import Checklist from './CheckList';
 
@@ -9,6 +9,9 @@ const Actividades = () => {
     return (
       <View style={styles.container}>
         <WelcomeBar/>
+        <View style={styles.Navbar}>
+        <Navbar />
+      </View>
         <TextInput style={styles.input} placeholder='Escriba para buscar el proyecto'></TextInput>
         <Checklist/>
       </View>
@@ -24,5 +27,20 @@ const styles = StyleSheet.create({
       margin: 10,
       borderRadius: 10
     },
+    
+ Navbar:{
+  margin:0,
+  marginBottom:40,
+  padding:0,
+  width:"100%",
+  height:"10%",
+  // backgroundColor:"rgba(170, 164, 164, 0.9)",
+  display:"flex",
+  justifyContent:"center",
+  alignItems:"center",
+  flexDirection:"row",
+  flexWrap:"wrap"
+
+},
   });
 export default Actividades
--- client/components/screens/indicadores/indicadores.js
+++ client/components/screens/indicadores/indicadores.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet  } from 'react-native';
 import Constants from "expo-constants";
-
+import Navbar from '../../Navbar/Navbar';
 import WelcomeBar from '../../welcomeBar';
 
 const Indicadores = () => {
     return (
         <View style={styles.container}>
             <WelcomeBar/>
+            <View style={styles.Navbar}>
+        <Navbar />
+      </View>
             <TextInput style={styles.input} placeholder='Busca el Proyecto o sin Proyecto'/>
             <TextInput style={styles.input} placeholder='Fecha inicio - Fecha fin'/>
             <View style={styles.inputCont}>
@@ -68,6 +71,21 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center"
     },
+    Navbar:{
+        margin:0,
+        marginBottom:40,
+        padding:0,
+        width:"100%",
+        height:"10%",
+        // backgroundColor:"rgba(170, 164, 164, 0.9)",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        flexDirection:"row",
+        flexWrap:"wrap"
+  
+      },
+  
   });
 
 export default Indicadores 
