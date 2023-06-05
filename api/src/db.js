48b25ad34d9c6111e8e62e7c6cd3f95f778d5689
estiven2111 <estiven.a.martinez@gmail.com>
2023-06-05 15:05:42 -0500

    sql proyectos componentes avtividades

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,7 @@ const path = require("path");
 const sequelize = new Sequelize('Fritomania', 'estiven2111_SQLLogin_1', 'lxsl4f4uji', {
   host: 'Fritomania.mssql.somee.com',
   dialect: 'mssql', /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-port:"1433"
+// port:"1433"
 });
 
 const basename = path.basename(__filename);
--- client/components/screens/actividades/actividades.js
+++ client/components/screens/actividades/actividades.js
@@ -1,21 +1,37 @@
-import React from "react";
-import { ScrollView, View, TextInput, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, View, TextInput, StyleSheet,FlatList } from "react-native";
 import Constants from "expo-constants";
 import Navbar from "../../Navbar/Navbar";
 import WelcomeBar from "../../welcomeBar";
 import Checklist from "./CheckList";
+import api from "../../../api/api";
 
 const Actividades = () => {
+const [search,setSearch] = useState([]);
+  const handlerSearch = async (search) =>{
+    const response = await api.post("/proyect/search", {search});
+    // const searchs = response.data.map(pro=>pro.proyect)
+    setSearch(searchs)
+    
+  }
   return (
     <ScrollView style={styles.container}>
       <WelcomeBar />
       <View style={styles.Navbar}>
         <Navbar />
       </View>
+      <View>
       <TextInput
         style={styles.input}
         placeholder="Escriba para buscar el proyecto"
+        onChangeText={handlerSearch}
       ></TextInput>
+      {/* <FlatList
+        data={search}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        keyExtractor={(item) => item}
+      /> */}
+      </View>
       <Checklist />
     </ScrollView>
   );
