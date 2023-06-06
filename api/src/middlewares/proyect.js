923370d92f8303e9fb437dfb6b9936869129b677
estiven2111 <estiven.a.martinez@gmail.com>
2023-06-06 10:39:17 -0500

    Search Proyect Ok

--- api/src/middlewares/proyect.js
+++ api/src/middlewares/proyect.js
@@ -2,130 +2,248 @@ const { Op } = require("sequelize");
 const { Proyect, Componet, Activity, sequelize } = require("../db");
 const { LocalStorage } = require("node-localstorage");
 const localStorage = new LocalStorage("./local-storage");
-const proyect = async (req, res) => {
-  const { proyect, description } = req.body;
+// const proyect = async (req, res) => {
+//   const { proyect, description } = req.body;
 
-  await Proyect.create({ proyect, description });
+//   await Proyect.create({ proyect, description });
 
-  res.send("ok");
-};
+//   res.send("ok");
+// };
 
-const componet = async (req, res) => {
-  // const { componet, proy_id } = req.body;
-  // const proy = await Componet.create({ componet });
-  // proy.setProyect(proy_id);
+// const componet = async (req, res) => {
+//   // const { componet, proy_id } = req.body;
+//   // const proy = await Componet.create({ componet });
+//   // proy.setProyect(proy_id);
 
-  // res.send(proy);
-  localStorage.removeItem("proyect")
-  res.json("proyecto eliminado del localstorage")
-};
+//   // res.send(proy);
+//   // localStorage.removeItem("proyect");
 
-const activity = async (req, res) => {
-  const { activity, description, com_id } = req.body;
-  const acti = await Activity.create({ activity, description });
-  acti.setComponet(com_id);
+//   localStorage.clear();
+//   res.json("proyecto eliminado del localstorage");
+// };
 
-  res.json(acti);
-};
+// const activity = async (req, res) => {
+//   const { activity, description, com_id } = req.body;
+//   const acti = await Activity.create({ activity, description });
+//   acti.setComponet(com_id);
 
-const getComponet = async (req, res) => {
-  // todo esta consulta si sirve para enviar todos los proyector
-  // const conse = await Proyect.findAll({
-  //   include: {
-  //     model: Componet,
-  //     include: Activity,
-  //   },
-  // });
-  // todo *************************************
+//   res.json(acti);
+// };
 
+// //! se puede borrar este metodo eliminar hijos
+// const getComponet = async (req, res) => {
+//   // todo esta consulta si sirve para enviar todos los proyector
+//   // const conse = await Proyect.findAll({
+//   //   include: {
+//   //     model: Componet,
+//   //     include: Activity,
+//   //   },
+//   // });
+//   // todo *************************************
+
+//   //? se piden los datos del usuario en el localStorage
+//   const user = JSON.parse(localStorage.getItem("user"));
+
+//   //? se selecciona los idNodo para saber que proyectos tiene el usuario
+//   const idnodo = await sequelize.query(
+//     `SELECT idNodoProyecto FROM TBL_SER_ProyectoActividadesEmpleados where N_DocumentoEmpleado = ${user.Doc_id} `
+//   );
+
+//   let proyect;
+//   let obj_proyecto = {
+//     proyectos: [],
+//   };
+//   let con = 0;
+
+//   for (const i of idnodo[0]) {
+//     proyect = await sequelize.query(
+//       `SELECT * FROM TBL_SER_PROYECTOS WHERE SKU IN (SELECT DISTINCT(SKU_Proyecto) FROM TBL_SER_ProyectoActividadesEmpleados WHERE N_DocumentoEmpleado = :docId AND idNodo = ${i.idNodoProyecto}) ORDER BY sku, idNodo`,
+//       { replacements: { docId: user.Doc_id } }
+//     );
+//     let idPadre = proyect[0][0].idPadre;
+//     let tipoParte;
+//     let Parte = idPadre;
+//     let actividad = "";
+//     let componente = "";
+//     let proyecto = "";
+//     if (proyect[0][0].TipoParte === "Actividad") {
+//       actividad = proyect[0][0].Nombre;
+//     }
+//     do {
+//       tipoParte = await sequelize.query(
+//         `SELECT * FROM TBL_SER_PROYECTOS WHERE SKU IN (SELECT DISTINCT(SKU_Proyecto) FROM TBL_SER_ProyectoActividadesEmpleados WHERE N_DocumentoEmpleado = :docId AND idNodo = ${Parte}) ORDER BY sku, idNodo`,
+//         { replacements: { docId: user.Doc_id } }
+//       );
+//       Parte = tipoParte[0][0].idPadre;
+//       if (tipoParte[0][0].TipoParte === "PP") {
+//         componente = tipoParte[0][0].Nombre;
+//       }
+//       if (tipoParte[0][0].TipoParte === "Cabecera") {
+//         proyecto = tipoParte[0][0].Nombre;
+//       }
+//       // Verificar si el proyecto ya existe en el objeto obj_proyecto
+//       let proyectoExistente = obj_proyecto.proyectos.find(
+//         (p) => p.proyecto === proyecto
+//       );
+
+//       if (proyectoExistente) {
+//         // Verificar si el componente ya existe en el proyecto
+//         let componenteExistente = proyectoExistente.componentes.find(
+//           (c) => c.componente === componente
+//         );
+
+//         if (componenteExistente) {
+//           // Agregar la actividad al componente existente
+//           componenteExistente.actividades.push({ actividad: actividad });
+//         } else {
+//           // Agregar un nuevo componente con la actividad al proyecto existente
+//           proyectoExistente.componentes.push({
+//             componente: componente,
+//             actividades: [{ actividad: actividad }],
+//           });
+//         }
+//       } else {
+//         // Agregar un nuevo proyecto con el componente y actividad
+//         if (proyecto !== "") {
+//           obj_proyecto.proyectos?.push({
+//             proyecto: proyecto,
+//             componentes: [
+//               {
+//                 componente: componente,
+//                 actividades: [{ actividad: actividad }],
+//               },
+//             ],
+//           });
+//         }
+//       }
+//     } while (tipoParte[0][0].TipoParte !== "Cabecera");
+//   }
+
+//   localStorage.setItem("proyect", JSON.stringify(obj_proyecto));
+//   res.json(obj_proyecto);
+// };
+
+const LoadProyect = async (Doc_id) => {
   //? se piden los datos del usuario en el localStorage
-  const user = JSON.parse(localStorage.getItem("user"));
-
-  //? se selecciona los idNodo para saber que proyectos tiene el usuario 
+  // const user = JSON.parse(localStorage.getItem("user"));
+  console.log(Doc_id);
+  //? se selecciona los idNodo para saber que proyectos tiene el usuario
   const idnodo = await sequelize.query(
-    `SELECT idNodoProyecto FROM TBL_SER_ProyectoActividadesEmpleados where N_DocumentoEmpleado = ${user.Doc_id} `
+    `SELECT idNodoProyecto FROM TBL_SER_ProyectoActividadesEmpleados where N_DocumentoEmpleado = ${Doc_id} `
   );
 
   let proyect;
   let obj_proyecto = {
-    proyectos:[]
+    proyectos: [],
   };
   let con = 0;
-  
+
   for (const i of idnodo[0]) {
     proyect = await sequelize.query(
       `SELECT * FROM TBL_SER_PROYECTOS WHERE SKU IN (SELECT DISTINCT(SKU_Proyecto) FROM TBL_SER_ProyectoActividadesEmpleados WHERE N_DocumentoEmpleado = :docId AND idNodo = ${i.idNodoProyecto}) ORDER BY sku, idNodo`,
-      { replacements: { docId: user.Doc_id } }
+      { replacements: { docId: Doc_id } }
     );
     let idPadre = proyect[0][0].idPadre;
     let tipoParte;
     let Parte = idPadre;
-    let actividad  = "";
-    let componente  = "";
-    let proyecto  = "";
+    let actividad = "";
+    let componente = "";
+    let proyecto = "";
     if (proyect[0][0].TipoParte === "Actividad") {
-      actividad  = proyect[0][0].Nombre
+      actividad = proyect[0][0].Nombre;
     }
     do {
       tipoParte = await sequelize.query(
         `SELECT * FROM TBL_SER_PROYECTOS WHERE SKU IN (SELECT DISTINCT(SKU_Proyecto) FROM TBL_SER_ProyectoActividadesEmpleados WHERE N_DocumentoEmpleado = :docId AND idNodo = ${Parte}) ORDER BY sku, idNodo`,
-        { replacements: { docId: user.Doc_id } }
+        { replacements: { docId: Doc_id } }
       );
       Parte = tipoParte[0][0].idPadre;
       if (tipoParte[0][0].TipoParte === "PP") {
-        componente  = tipoParte[0][0].Nombre;
+        componente = tipoParte[0][0].Nombre;
       }
       if (tipoParte[0][0].TipoParte === "Cabecera") {
-        proyecto  = tipoParte[0][0].Nombre;
+        proyecto = tipoParte[0][0].Nombre;
       }
- // Verificar si el proyecto ya existe en el objeto obj_proyecto
-  let proyectoExistente = obj_proyecto.proyectos.find((p) => p.proyecto === proyecto);
+      // Verificar si el proyecto ya existe en el objeto obj_proyecto
+      let proyectoExistente = obj_proyecto.proyectos.find(
+        (p) => p.proyecto === proyecto
+      );
 
-if (proyectoExistente) {
-   // Verificar si el componente ya existe en el proyecto
-    let componenteExistente = proyectoExistente.componentes.find((c) => c.componente === componente);
+      if (proyectoExistente) {
+        // Verificar si el componente ya existe en el proyecto
+        let componenteExistente = proyectoExistente.componentes.find(
+          (c) => c.componente === componente
+        );
 
-    if (componenteExistente) {
-      // Agregar la actividad al componente existente
-      componenteExistente.actividades.push({ actividad: actividad });
-    } else {
-     // Agregar un nuevo componente con la actividad al proyecto existente
-      proyectoExistente.componentes.push({
-        componente: componente,
-        actividades: [{ actividad: actividad }]
-      });
-    }
-  } else {
-    // Agregar un nuevo proyecto con el componente y actividad
-   if (proyecto !== "") {
-    obj_proyecto.proyectos?.push({
-      proyecto: proyecto,
-      componentes: [{
-        componente: componente,
-        actividades: [{ actividad: actividad }]
-      }]
-    });
-  }
-   }
+        if (componenteExistente) {
+          // Agregar la actividad al componente existente
+          componenteExistente.actividades.push({ actividad: actividad });
+        } else {
+          // Agregar un nuevo componente con la actividad al proyecto existente
+          proyectoExistente.componentes.push({
+            componente: componente,
+            actividades: [{ actividad: actividad }],
+          });
+        }
+      } else {
+        // Agregar un nuevo proyecto con el componente y actividad
+        if (proyecto !== "") {
+          obj_proyecto.proyectos?.push({
+            proyecto: proyecto,
+            componentes: [
+              {
+                componente: componente,
+                actividades: [{ actividad: actividad }],
+              },
+            ],
+          });
+        }
+      }
     } while (tipoParte[0][0].TipoParte !== "Cabecera");
   }
 
-  localStorage.setItem("proyect", JSON.stringify(obj_proyecto))
-  res.json(obj_proyecto);
+  localStorage.setItem(`Proyecto`, JSON.stringify(obj_proyecto));
+  //! en el deploy validar que el archivo no se sobreescriba
 };
 
 //todo hacer consulta para proyectos enviando respuesta automatica
 const getProyectName = async (req, res) => {
   const { search } = req.body;
-  console.log(search);
-  const searchs = await Proyect.findAll({
-    where: {
-      proyect: { [Op.substring]: `%${search}%` },
-    },
-  });
-  res.json(searchs);
+  const proyects = JSON.parse(localStorage.getItem(`Proyecto`));
+  // localStorage.removeItem(`Proyecto`)
+  const NomProyect = proyects.proyectos
+    .filter((obj) => obj.proyecto.includes(search.toUpperCase()))
+    .map((obj) => obj.proyecto);
+
+  if (NomProyect.length <= 0) {
+    return res.json("No hay royectos con este nombre ");
+  }
+  // const proyect = proyects.proyectos.filter((obj) => {
+  //   return obj.proyecto.includes(search);
+  // });
+  res.json(NomProyect);
 };
+
+//todo hacer consulta para enviar el proyectos
+const getProyect = async (req, res) => {
+  const { search } = req.body;
+  const proyects = JSON.parse(localStorage.getItem(`Proyecto`));
+  //? me devuelve todo el objeto
+  const proyect = proyects.proyectos.filter((obj) => {
+    return obj.proyecto.includes(search.toUpperCase());
+  });
+  res.json(proyect);
+};
+
 //todo debe enviar el arbol del proyecto con componente y actividad y verificar si requiere entregables
 //todo hacer el post de entregable
 
-module.exports = { proyect, componet, getComponet, activity, getProyectName };
+module.exports = {
+  // proyect,
+  // componet,
+  //  getComponet,
+  //  activity,
+  getProyectName,
+  getProyect,
+  LoadProyect,
+};
--- api/src/routes/proyectRouter.js
+++ api/src/routes/proyectRouter.js
@@ -1,17 +1,19 @@
 const { Router } = require("express");
 const ProyectRouter = Router();
 const {
-  proyect,
-  componet,
-  getComponet,
-  activity,
+  // proyect,
+  // componet,
+  // getComponet,
+  // activity,
   getProyectName,
+  getProyect
 } = require("../middlewares/index");
 
-ProyectRouter.post("/", proyect);
-ProyectRouter.post("/com", componet);
-ProyectRouter.get("/com", getComponet);
-ProyectRouter.post("/act", activity);
+// ProyectRouter.post("/", proyect);
+// ProyectRouter.post("/com", componet);
+// ProyectRouter.get("/com", getComponet);
+// ProyectRouter.post("/act", activity);
 ProyectRouter.post("/search", getProyectName);
+ProyectRouter.post("/", getProyect);
 
 module.exports = ProyectRouter;
