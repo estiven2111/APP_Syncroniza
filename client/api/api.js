bb833d107b5feead61a3056dd02026f686dfc5f0
XCODE89 <maph0989@gmail.com>
2023-06-20 10:50:36 -0400

    prueba finalizada

--- client/api/api.js
+++ client/api/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   // baseURL: 'http://192.168.1.7:5000/user/api', // Reemplaza con la URL de tu backend Estive
   // baseURL: 'http://192.168.0.146:5000/user/api', // Reemplaza con la URL de tu backend Miguel
   baseURL: 'http://192.168.1.176:5000/user/api', // Reemplaza con la URL de tu backend Miguel Chilegit push origin
-  cambio de prueba
   
 });
 
