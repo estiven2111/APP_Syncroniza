a0e3bd8b4b9c7e9bcbb7e277503ff141858e70a3
estiven2111 <estiven.a.martinez@gmail.com>
2023-06-27 18:46:48 -0500

    ocr

--- api/src/middlewares/Ocr.js
+++ api/src/middlewares/Ocr.js
@@ -1,18 +1,18 @@
 const Tesseract = require("tesseract.js");
 const path = require("path");
 const fs_extra = require("fs-extra");
+const { createCanvas, loadImage } = require("canvas");
 // const multer = require('multer')
 const Ocr = async (req, res) => {
   // await fs_extra.unlink(`./Conversor-de-imagen-a-texto-en-Chrome (1).png`);
   let imagen;
   let uploadPath;
   imagen = req.files.imagen;
-  uploadPath = `${imagen.name}`; //? uploads que estatica por ende se van aguardar ahi ruta uploads/nom
+  uploadPath = `src/middlewares/${imagen.name}`; //? uploads que estatica por ende se van aguardar ahi ruta uploads/nom
   console.log(imagen);
-  console.log("sssssssssssssssssss", uploadPath);
-  res.send("enviado")
-  return
-  imagen.mv(uploadPath, (err) => {
+  //console.log("sssssssssssssssssss", uploadPath);
+
+  imagen.mv(`${uploadPath}`, (err) => {
     if (err) return res.status(500).send(err);
   });
 
@@ -38,19 +38,33 @@ const Ocr = async (req, res) => {
   //     (`../../../${imagen.name}`)
   //   );
 
-  const { createWorker } = Tesseract;
+  try {
+    const { createWorker } = Tesseract;
 
-  const worker = await createWorker();
+    const worker = await createWorker();
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const {
+      data: { text },
+    } = await worker.recognize(`src/middlewares/${imagen.name}`);
 
-  await worker.loadLanguage("eng");
-  await worker.initialize("eng");
-  const {
-    data: { text },
-  } = await worker.recognize(image);
+    await worker.terminate();
+    // await fs_extra.unlink(`src/middlewares/${imagen.name}`);
+    //   const regex = /saldo:(\d+)/;
+    // const match = data.text.match(regex);
 
-  await worker.terminate();
-
-  res.send(text);
+    // if (match) {
+    //   // El número se encuentra en el grupo de captura (índice 1)
+    //   const numero = match[1];
+    //   console.log("Número de saldo:", numero);
+    // } else {
+    //   console.log("No se encontró un número de saldo");
+    // }
+    res.send(text);
+  } catch (error) {
+    res.send(error);
+  }
   // todo esto es otro
 
   //   const { createWorker, PSM } = Tesseract;
--- client/api/api.js
+++ client/api/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://192.168.1.7:5000/user/api', // Reemplaza con la URL de tu backend Estive
+   baseURL: 'http://192.168.1.11:5000/user/api', // Reemplaza con la URL de tu backend Estive
   // baseURL: 'http://192.168.0.146:5000/user/api', // Reemplaza con la URL de tu backend Miguel
-   baseURL: 'https://appsyncroniza-production.up.railway.app/user/api' //! URL PARA DEPLOY
+  //baseURL: 'https://appsyncroniza-production.up.railway.app/user/api' //! URL PARA DEPLOY
   //baseURL: 'http://192.168.1.176:5000/user/api', // Reemplaza con la URL de tu backend Miguel Chile
  // baseURL: 'http://192.168.1.176:5000/user/api', // Reemplaza con la URL de tu backend Miguel Chile
   
