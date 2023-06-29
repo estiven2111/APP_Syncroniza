f01413df213e4d7c7396544973225d9f7a93968e
estiven2111 <estiven.a.martinez@gmail.com>
2023-06-28 22:15:56 -0500

    pruebas ocr

--- api/src/middlewares/Ocr.js
+++ api/src/middlewares/Ocr.js
@@ -4,12 +4,16 @@ const fs_extra = require("fs-extra");
 
 const { Client } = require("@microsoft/microsoft-graph-client");
 const fs = require("fs");
+const Jimp = require("jimp"); //! organizar pixeles de imagenes
+const pdf2pic = require("pdf2pic"); //!convertir de pdf a jpg
+
 
 // const { createCanvas, loadImage } = require("canvas");
 // const multer = require('multer')
 const Ocr = async (req, res) => {
   // await fs_extra.unlink(`./Conversor-de-imagen-a-texto-en-Chrome (1).png`);
   const { img } = req.files;
+  console.log(img)
   let imagen;
   let uploadPath;
   imagen = req.files.img;
@@ -19,66 +23,126 @@ const Ocr = async (req, res) => {
   });
 
   try {
-    const { createWorker } = Tesseract;
-    const worker = await createWorker();
-    await worker.load();
-    await worker.loadLanguage("eng");
-    await worker.initialize("eng");
-    const {
-      data: { text },
-    } = await worker.recognize(`uploads/${imagen.name}`);
-    await worker.terminate();
-//   const inputText = `FIERRO 8 BURGER ESPERANZA
-// NET/Doe: 1.121.944.208
-// Lair.
-// Tel. 3123022442
-// Factura de venta: No. 12049
-// Fecha Factura: 10/11/2022 08:39:38 p. m. :
-// Cliente: craams
-// Tipo de L scumpnio: NIT
-// Nimero de Documento: 811007547-0
-// D cior 3 40 #56-11 medellin
-// Medic de pago: Elccl
-// Atendide Par: MARTHA
-// Mesa: Donucilios 1
-// Cantidad UN Producto Precio fotal
-// i Und. FAFA FRANCESA § 4.500 $ 4.500
-// ! Und, DON JOSE $ 20.000 § 20.000
-// VALOR A PAGAR:
-// ) $24.500`;
-//! EXPRESSIONES REGULARES
-const regexNetDoe = /NET\/Doe:\s+(.+)/;
-const regexFacturaVenta = /Factura de venta: No.\s+(.+)/;
-const regexNumeroDocumento = /Nimero de Documento:\s+(.+)/;
-// const regexValorAPagar = /VALOR A PAGAR:\)\s+\$(.+)/;
-const regexValorAPagar = /\$([\d,.]+)/;
-const regexNombre = /FIERRO 8 BURGER ESPERANZA/;
+    //! convertir pdf a jpg  pdfPath=ruta donde esta la img, page= la 1 o la pagina que se desea convertir
+    // const converter = pdf2pic.default({
+    //   density: 100,   // Densidad de la imagen en DPI (puntos por pulgada)
+    //   format: 'jpg',  // Formato de imagen de salida
+    //   size: '600x800', // Tamaño de la imagen de salida (ancho x alto)
+    //   quality: 100    // Calidad de la imagen de salida (valor de 0 a 100)
+    // });
 
-const matchNetDoe = text.match(regexNetDoe);
-const matchFacturaVenta = text.match(regexFacturaVenta);
-const matchNumeroDocumento = text.match(regexNumeroDocumento);
-const matchValorAPagar = text.match(regexValorAPagar);
-const matchNombre=text.match(regexNombre)
+    // const result = await converter.convert(pdfPath, page, '__output.jpg');
+    // return result.path;
 
-const netDoe = matchNetDoe ? matchNetDoe[1] : null;
-const facturaVenta = matchFacturaVenta ? matchFacturaVenta[1] : null;
-const numeroDocumento = matchNumeroDocumento ? matchNumeroDocumento[1] : null;
-const valorAPagar = matchValorAPagar ? matchValorAPagar[1] : null;
-const Nombre = matchNombre ? matchNombre[0] : null;
-console.log(Nombre)
-const obj = {
-  netDoe,
-  facturaVenta,numeroDocumento,valorAPagar,Nombre
-}
-console.log("NET/Doe:", netDoe);
-console.log("Factura de venta: No.", facturaVenta);
-console.log("Nimero de Documento:", numeroDocumento);
-console.log("VALOR A PAGAR:) $", valorAPagar);
-res.send(obj)
+    Jimp.read(`uploads/${imagen.name}`, (err, image) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      // Aquí puedes realizar las mejoras en la imagen
+
+      // Obtener los píxeles de la imagen
+      image.scan(
+        0,
+        0,
+        image.bitmap.width,
+        image.bitmap.height,
+        function (x, y, idx) {
+          // Obtener los valores de los canales de color (rojo, verde, azul) del píxel actual
+          // const red = this.bitmap.data[idx + 0];
+          // const green = this.bitmap.data[idx + 1];
+          // const blue = this.bitmap.data[idx + 2];
+          // const black = this.bitmap.data[idx];
+          // Ajustar los niveles de color
+          // const threshold = 100; // Valor de umbral para determinar los límites de ajuste
+          // // Si el valor del canal de color negro supera el umbral, se ajusta a un valor mayor
+          // if (black > threshold) {
+          //   this.bitmap.data[idx] = 255; // Máximo valor para el canal de color negro
+          // }
+          // // Si el canal rojo es mayor que el umbral, se ajusta a un valor máximo
+          //   if (red > threshold) {
+          //     this.bitmap.data[idx + 0] = 100; // Máximo valor para el canal rojo
+          //   }
+          //   // Si el canal verde es mayor que el umbral, se ajusta a un valor mínimo
+          //   if (green > threshold) {
+          //     this.bitmap.data[idx + 1] = 0; // Mínimo valor para el canal verde
+          //   }
+          //   // Si el canal azul es mayor que el umbral, se ajusta a un valor mínimo
+          //   if (blue > threshold) {
+          //     this.bitmap.data[idx + 2] = 0; // Mínimo valor para el canal azul
+          //   }
+        }
+      );
+
+      // Ajustar brillo y contraste
+      image.brightness(0.1).contrast(0.2);
+
+      // // Ecualizar el histograma
+     
+      image.normalize();
+
+      // // Aplicar filtrado de ruido
+        // image.gaussian(1);
+
+      // // Mejorar nitidez
+      image.convolute([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]);
+
+      // // Corregir el balance de blancos
+      // image.color([{ apply: 'desaturate', params: [20] }]);
+
+      image.write(`uploads/proce${imagen.name}`, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        console.log("Imagen procesada guardada correctamente.");
+      });
+    });
+
+    //     const { createWorker } = Tesseract;
+    //     const worker = await createWorker();
+    //     await worker.load();
+    //     await worker.loadLanguage("eng");
+    //     await worker.initialize("eng");
+    //     const {
+    //       data: { text },
+    //     } = await worker.recognize(`uploads/${imagen.name}`);
+    //     await worker.terminate();
+
+    // //! EXPRESSIONES REGULARES
+    // const regexNetDoe = /NET\/Doe:\s+(.+)/;
+    // const regexFacturaVenta = /Factura de venta: No.\s+(.+)/;
+    // const regexNumeroDocumento = /Nimero de Documento:\s+(.+)/;
+    // // const regexValorAPagar = /VALOR A PAGAR:\)\s+\$(.+)/;
+    // const regexValorAPagar = /\$([\d,.]+)/;
+    // const regexNombre = /FIERRO 8 BURGER ESPERANZA/;
+
+    // const matchNetDoe = text.match(regexNetDoe);
+    // const matchFacturaVenta = text.match(regexFacturaVenta);
+    // const matchNumeroDocumento = text.match(regexNumeroDocumento);
+    // const matchValorAPagar = text.match(regexValorAPagar);
+    // const matchNombre=text.match(regexNombre)
+
+    // const netDoe = matchNetDoe ? matchNetDoe[1] : null;
+    // const facturaVenta = matchFacturaVenta ? matchFacturaVenta[1] : null;
+    // const numeroDocumento = matchNumeroDocumento ? matchNumeroDocumento[1] : null;
+    // const valorAPagar = matchValorAPagar ? matchValorAPagar[1] : null;
+    // const Nombre = matchNombre ? matchNombre[0] : null;
+    // console.log(Nombre)
+    // const obj = {
+    //   netDoe,
+    //   facturaVenta,numeroDocumento,valorAPagar,Nombre
+    // }
+    // console.log("NET/Doe:", netDoe);
+    // console.log("Factura de venta: No.", facturaVenta);
+    // console.log("Nimero de Documento:", numeroDocumento);
+    // console.log("VALOR A PAGAR:) $", valorAPagar);
+    res.send("text");
   } catch (error) {
-    return res.send(error)
+    return res.send(error);
   }
- 
 };
 
 //!validar onedrive
