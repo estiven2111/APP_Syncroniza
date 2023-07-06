b119c561026ea3a573c73b83d05dc5a35a33fca8
estiven2111 <estiven.a.martinez@gmail.com>
2023-07-05 21:34:53 -0500

    OCR AZURE

--- api/src/routes/proyectRouter.js
+++ api/src/routes/proyectRouter.js
@@ -10,12 +10,13 @@ const {
 } = require("../middlewares/index");
 
 
-
+const { ComputerVisionClient, AzureKeyCredential } = require("@azure/cognitiveservices-computervision");
 ProyectRouter.get("/search", getProyectName);
 ProyectRouter.get("/", getProyect);
 ProyectRouter.get("/logout", logout);
 ProyectRouter.post("/hours", registerActivities);
 ProyectRouter.get("/hours", hourActivities);
 ProyectRouter.post("/ocr",Ocr)
+
 //,fileupload({ useTempFiles: true, tempFileDir: "./uploads" })
 module.exports = ProyectRouter;
